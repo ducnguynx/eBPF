74cd21ce39a1fa93c7c31e44f51198a03f2c52a8
ducnguynx <nganhduc2376@gmail.com>
2024-10-21 16:53:46 +0700

    Update hello_usr.c

--- hello_usr.c
+++ hello_usr.c
@@ -7,13 +7,13 @@
 struct key {
     __u8 address[6];
 };
-
 struct value {
-    __u64 timesAppear;
+    __u64 timesAppearDest;
+    __u64 timesAppearSource;
 };
 
 int main() {
-    int map_fd = bpf_obj_get("/sys/fs/bpf/xdp_map_count"); // Path to pinned map
+    int map_fd = bpf_obj_get("/sys/fs/bpf/xdp_map_count1"); // Path to pinned map
     if (map_fd < 0) {
         perror("bpf_obj_get");
         return 1;
@@ -22,18 +22,21 @@ int main() {
     struct key cur_key = {};
     struct key next_key;
     struct value val;
-
+    while (1) {
+    cur_key = (struct key) {} ;
     // Traverse the map using bpf_map_get_next_key() and bpf_map_lookup_elem()
     while (bpf_map_get_next_key(map_fd, &cur_key, &next_key) == 0) {
         if (bpf_map_lookup_elem(map_fd, &next_key, &val) == 0) {
-            printf("Address %02x:%02x:%02x:%02x:%02x:%02x, Times %llu\n",
+            printf("Address %02x:%02x:%02x:%02x:%02x:%02x, Times Dest %llu,Times Src %llu\n",
                    next_key.address[0], next_key.address[1], next_key.address[2],
                    next_key.address[3], next_key.address[4], next_key.address[5],
-                   val.timesAppear);
+                   val.timesAppearDest,val.timesAppearSource);
         }
         cur_key = next_key;  // Move to the next key
     }
-
+    printf("\n----------------------\n");
+    sleep(1);
+    }
     close(map_fd);
     return 0;
 }
